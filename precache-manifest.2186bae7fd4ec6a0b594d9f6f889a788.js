3e8c4d76fc1e1318ad838ae2af9049a915fef090
Jiban Kumar Debashis Behera <66745177+jibanblog@users.noreply.github.com>
2020-08-01 20:04:12 +0530

    Updates

---
+++ precache-manifest.2186bae7fd4ec6a0b594d9f6f889a788.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9fa4cbedf0457694ba2f80e6b1392d",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "0f2911d9ad696b8c9e33",
+    "url": "/myapp/static/css/main.5283f36d.chunk.css"
+  },
+  {
+    "revision": "d58ac42dee467db12ca7",
+    "url": "/myapp/static/js/2.16828230.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/myapp/static/js/2.16828230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f2911d9ad696b8c9e33",
+    "url": "/myapp/static/js/main.dabafa37.chunk.js"
+  },
+  {
+    "revision": "f934ef5e45688dd4b94a",
+    "url": "/myapp/static/js/runtime-main.6dea025d.js"
+  },
+  {
+    "revision": "7432aff88efed2ff1bdffd0adb9a7f21",
+    "url": "/myapp/static/media/welcome.7432aff8.gif"
+  }
+]);
